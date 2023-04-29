78e46a3a339ea570db0fc8905b4fe672493cad6b
Fabio Krakauer <frk@cesar.org.br>
2023-04-29 17:23:55 -0300

    insert author and update ddl

--- etl.py
+++ etl.py
@@ -11,15 +11,31 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
-for line in open("minified-ddl.sql"):
-    print(line)
-    mycursor.execute(line)
+# for line in open("minified-ddl.sql"):
+#     # print(line)
+#     mycursor.execute(line)
 
-# doc = dict()
+doc = dict()
 
-# with open('curriculos/cesar-franca.xml') as fd:
-#     doc = xmltodict.parse(fd.read())
+ids = {
+    "author_id": None
+}
+querys = {
+    "insert_author": "INSERT INTO autores (nome_completo,resumo_cv,colaborador_cesar) VALUES (%s,%s, %s);"
+}
 
-# with open('cv.json', 'w') as output:
-#     json.dump(doc, output)
+with open('curriculos/cesar-franca.xml') as fd:
+    doc = xmltodict.parse(fd.read())
+
+    doc_cv = doc["CURRICULO-VITAE"]
+    general_data = doc_cv["DADOS-GERAIS"]
+    author_data = (general_data["@NOME-COMPLETO"], general_data["RESUMO-CV"]["@TEXTO-RESUMO-CV-RH"], 1)
+    
+    cursor = db.cursor()
+    cursor.execute(querys["insert_author"], author_data)
+    db.commit()
+
+    ids["author_id"] = cursor.lastrowid
+
+print(ids)
 
